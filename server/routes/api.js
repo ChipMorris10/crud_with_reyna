1b919fe75b692d0b9814c669c429a67b393d62de
ChipMorris10 <goldcomac@mac.com>
2015-09-15 13:13:27 -0600

    define schema

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var LLama = require('../models/llamas.js');
+var Llama = require('../models/llamas.js');
 
 // Get all llamas
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
     res.send("hello");
 });
 
